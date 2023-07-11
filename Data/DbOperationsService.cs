7d317fa8fb6f572244884763e507dc44d2daafb7
Pramod <emailforcoding123@gmail.com>
2023-07-11 09:06:27 -0400

    Sqlite database added note - not using efcore as there is only one entity and the db operations are very simple.
    Up next add a ui library and make some ui changes

---
+++ Data/DbOperationsService.cs
@@ -0,0 +1,49 @@
+﻿namespace Panulu.Data;
+
+using CloudKit;
+using Panulu.Entities;
+using SQLite;
+public class DbOperationsService {
+
+    SQLiteAsyncConnection Connection;
+
+    public DbOperationsService() {
+
+    }
+
+    private async Task Init() {
+        if (Connection is not null) {
+            return;
+        }
+
+        Connection = new SQLiteAsyncConnection(DatabaseConfig.DatabasePath, DatabaseConfig.Flags);
+        _ = await Connection.CreateTableAsync<TaskItem>();
+    }
+
+
+    public async Task<List<TaskItem>> GetItemsAsync() {
+        await Init();
+        return await Connection.Table<TaskItem>().ToListAsync();
+    }
+
+
+    public async Task<TaskItem> GetItemAsync(int id) {
+        await Init();
+        return await Connection.Table<TaskItem>().Where(i => i.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<int> SaveItemAsync(TaskItem item) {
+        await Init();
+        if (item.Id != 0)
+            return await Connection.UpdateAsync(item);
+        else
+            return await Connection.InsertAsync(item);
+    }
+
+    public async Task<int> DeleteItemAsync(TaskItem item) {
+        await Init();
+        return await Connection.DeleteAsync(item);
+    }
+
+
+}
