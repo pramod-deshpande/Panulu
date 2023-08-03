b9e9bcc4f59020ec015605d7a6322bbbae1f0759
Pramod <emailforcoding123@gmail.com>
2023-08-03 16:32:38 -0400

    Finished Logic for reverting tasks. Next - Enchancing UI

--- Services/TaskService.cs
+++ Services/TaskService.cs
@@ -1,8 +1,11 @@
-﻿using Panulu.Data;
+﻿using Microsoft.AspNetCore.Components;
+using Panulu.Data;
+using Panulu.Entities;
 
 namespace Panulu.Services {
     public class TaskService {
         private readonly DbOperationsService _db;
+
 
         public TaskService(DbOperationsService db) {
             _db = db;
@@ -18,6 +21,60 @@ namespace Panulu.Services {
             }
         }
 
+
+        //Get all pending tasks 
+        public async Task<List<TaskItem>> GetAllIncompleteTasks()
+        {
+            var items = await _db.GetItemsAsync();
+            await AssignDateForTasksWithoutDateAsync(items, DateTime.Now.Date);
+            return items.Where(x => !x.IsCompleted).OrderBy(x => x.TaskDateTime).ToList();
+        }
+
+
+
+        //Get today's tasks 
+
+        public async Task<List<TaskItem>> GetTasksForToday()
+        {
+            var items = await _db.GetItemsAsync();
+            await AssignDateForTasksWithoutDateAsync(items, DateTime.Now.Date);
+            return items.Where(x => x.TaskDateTime.Value.Date == DateTime.Now.Date && !x.IsCompleted).ToList();
+        }
+
+
+
+
+        //Assign date to tasks without a date
+        private async Task AssignDateForTasksWithoutDateAsync(List<TaskItem> items, DateTime date)
+        {
+            var tasksToUpdate = items.Where(x => !x.TaskDateTime.HasValue).ToList();
+            foreach (var item in tasksToUpdate)
+            {
+                item.TaskDateTime = date;
+            }
+
+            if (tasksToUpdate.Any())
+            {
+                foreach (var item in tasksToUpdate)
+                {
+                    await SaveTaskAsync(item);
+                }
+            }
+        }
+
+        //Save task 
+        public async Task SaveTaskAsync(TaskItem taskItem)
+        {
+            await _db.SaveItemAsync(taskItem);
+        }
+
+        public async Task RevertTaskAsync(TaskItem taskItem)
+        {
+            taskItem.IsCompleted = !taskItem.IsCompleted;
+            taskItem.TaskDateTime = DateTime.Now;
+            taskItem.CompletionDateTime = new DateTime();
+            await SaveTaskAsync(taskItem);
+        }
 
     }
 }
