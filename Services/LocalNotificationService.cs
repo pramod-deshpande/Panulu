3a70cb1bbf2e6e23b2f3a9def5fe830eead96a00
Pramod <emailforcoding123@gmail.com>
2023-08-14 15:44:27 -0400

    Working on Local Notifications

---
+++ Services/LocalNotificationService.cs
@@ -0,0 +1,38 @@
+﻿using Plugin.LocalNotification;
+
+namespace Panulu.Services;
+
+public class Notification
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Subtitle { get; set; }
+    public DateTime ScheduleDateTime { get; set; }
+    public bool Repeat { get; set; }
+    public int RepeatInterval { get; set; }
+
+
+    public void CreateNotification()
+    {
+        Random random = new();
+        int repeatInterval = 0;
+        if(Repeat)
+        {
+            repeatInterval = RepeatInterval;
+        }
+        var notification = new NotificationRequest()
+        {
+            Title = Title,
+            Description = Description,
+            Subtitle = Subtitle,
+            BadgeNumber = random.Next(),
+
+            Schedule = new NotificationRequestSchedule()
+            {
+                NotifyTime = ScheduleDateTime,
+                NotifyRepeatInterval = TimeSpan.FromDays(repeatInterval)
+            }
+        };
+        LocalNotificationCenter.Current.Show(notification);
+    }
+}
--- Services/TaskService.cs
+++ Services/TaskService.cs
@@ -1,80 +1,79 @@
-﻿using Microsoft.AspNetCore.Components;
-using Panulu.Data;
+﻿using Panulu.Data;
 using Panulu.Entities;
 
-namespace Panulu.Services {
-    public class TaskService {
-        private readonly DbOperationsService _db;
+namespace Panulu.Services;
+
+public class TaskService {
+    private readonly DbOperationsService _db;
 
 
-        public TaskService(DbOperationsService db) {
-            _db = db;
+    public TaskService(DbOperationsService db) {
+        _db = db;
+    }
+
+    public async Task CarryOverIncompleteTasksAsync() {
+        var date = DateTime.Now;
+        var tasks = await _db.GetItemsAsync();
+        var incompleteTasks = tasks.Where(x => !x.IsCompleted && x.TaskDateTime < date);
+        foreach (var task in incompleteTasks) {
+            task.TaskDateTime = date;
+            await _db.SaveItemAsync(task);
         }
-
-        public async Task CarryOverIncompleteTasksAsync() {
-            var date = DateTime.Now;
-            var tasks = await _db.GetItemsAsync();
-            var incompleteTasks = tasks.Where(x => !x.IsCompleted && x.TaskDateTime < date);
-            foreach (var task in incompleteTasks) {
-                task.TaskDateTime = date;
-                await _db.SaveItemAsync(task);
-            }
-        }
+    }
 
 
-        //Get all pending tasks 
-        public async Task<List<TaskItem>> GetAllIncompleteTasks()
+    //Get all pending tasks 
+    public async Task<List<TaskItem>> GetAllIncompleteTasks()
+    {
+        var items = await _db.GetItemsAsync();
+        await AssignDateForTasksWithoutDateAsync(items, DateTime.Now.Date);
+        return items.Where(x => !x.IsCompleted).OrderBy(x => x.TaskDateTime).ToList();
+    }
+
+    //Get today's tasks 
+
+    public async Task<List<TaskItem>> GetTasksForToday()
+    {
+        var items = await _db.GetItemsAsync();
+        await AssignDateForTasksWithoutDateAsync(items, DateTime.Now.Date);
+        return items.Where(x => x.TaskDateTime.Value.Date == DateTime.Now.Date && !x.IsCompleted).ToList();
+    }
+
+    //Assign date to tasks without a date
+    private async Task AssignDateForTasksWithoutDateAsync(List<TaskItem> items, DateTime date)
+    {
+        var tasksToUpdate = items.Where(x => !x.TaskDateTime.HasValue).ToList();
+        foreach (var item in tasksToUpdate)
         {
-            var items = await _db.GetItemsAsync();
-            await AssignDateForTasksWithoutDateAsync(items, DateTime.Now.Date);
-            return items.Where(x => !x.IsCompleted).OrderBy(x => x.TaskDateTime).ToList();
+            item.TaskDateTime = date;
         }
 
-
-
-        //Get today's tasks 
-
-        public async Task<List<TaskItem>> GetTasksForToday()
+        if (tasksToUpdate.Any())
         {
-            var items = await _db.GetItemsAsync();
-            await AssignDateForTasksWithoutDateAsync(items, DateTime.Now.Date);
-            return items.Where(x => x.TaskDateTime.Value.Date == DateTime.Now.Date && !x.IsCompleted).ToList();
-        }
-
-
-
-
-        //Assign date to tasks without a date
-        private async Task AssignDateForTasksWithoutDateAsync(List<TaskItem> items, DateTime date)
-        {
-            var tasksToUpdate = items.Where(x => !x.TaskDateTime.HasValue).ToList();
             foreach (var item in tasksToUpdate)
             {
-                item.TaskDateTime = date;
-            }
-
-            if (tasksToUpdate.Any())
-            {
-                foreach (var item in tasksToUpdate)
-                {
-                    await SaveTaskAsync(item);
-                }
+                await SaveTaskAsync(item);
             }
         }
-
-        //Save task 
-        public async Task SaveTaskAsync(TaskItem taskItem)
-        {
-            await _db.SaveItemAsync(taskItem);
-        }
-
-        public async Task RevertTaskAsync(TaskItem taskItem)
-        {
-            taskItem.IsCompleted = !taskItem.IsCompleted;
-            taskItem.TaskDateTime = DateTime.Now;
-            taskItem.CompletionDateTime = new DateTime();
-            await SaveTaskAsync(taskItem);
-        }
-
     }
+
+    //Save task 
+    public async Task SaveTaskAsync(TaskItem taskItem)
+    {
+        await _db.SaveItemAsync(taskItem);
+    }
+
+    public async Task<TaskItem> GetTaskItemAsync(int id)
+    {
+        return await _db.GetItemAsync(id);
+    }
+
+    public async Task RevertTaskAsync(TaskItem taskItem)
+    {
+        taskItem.IsCompleted = !taskItem.IsCompleted;
+        taskItem.TaskDateTime = DateTime.Now;
+        taskItem.CompletionDateTime = new DateTime();
+        await SaveTaskAsync(taskItem);
+    }
+
 }
